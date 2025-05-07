b8b7548ce259dea3d7c80f6641ed36dc535476b4
JongMiiiin <912jongmin46@gmail.com>
2025-05-07 16:16:13 +0900

    Update App.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
@@ -13,26 +12,33 @@ import ReviewWrite from './pages/ReviewWrite';
 import ReviewView from './pages/ReviewView';
 import Profile from './pages/Profile';
 import StadiumTip from "./pages/StadiumTip";
+import Ranking from './pages/Ranking'; // 컴포넌트 import
+
+// ✅ 테마 컨텍스트 추가
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="diary" element={<Diary />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="excitingzone" element={<ExcitingZone />} />
-          <Route path="/guidezone" element={<GuideZone />} />
-          <Route path="/stadium/:id" element={<StadiumDetail />} />
-          <Route path="review/write" element={<ReviewWrite />} />
-          <Route path="review/view" element={<ReviewView />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/stadium/tip" element={<StadiumTip />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="diary" element={<Diary />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="excitingzone" element={<ExcitingZone />} />
+            <Route path="/guidezone" element={<GuideZone />} />
+            <Route path="/stadium/:id" element={<StadiumDetail />} />
+            <Route path="review/write" element={<ReviewWrite />} />
+            <Route path="review/view" element={<ReviewView />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/stadium/tip" element={<StadiumTip />} />
+            <Route path="/ranking" element={<Ranking />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
